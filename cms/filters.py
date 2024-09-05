9f75986db741b4c10745b6da88fbf2fe78336630
KingEddeh <magtoto.edward@gmail.com>
2024-09-05 18:59:35 +0800

    Fixed Medical Certificate Search

--- cms/filters.py
+++ cms/filters.py
@@ -1,6 +1,6 @@
 import django_filters
-
 from .models import *
+
 
 class PatientFilter(django_filters.FilterSet):
     class Meta:
@@ -17,13 +17,19 @@ class PatientFilter(django_filters.FilterSet):
         request.session['filtered_queryset'] = list(filtered_queryset.values_list('id', flat=True))
         return super().get(request, *args, **kwargs)
 
+
 class MedcertFilter(django_filters.FilterSet):
-    
-    unique_number = django_filters.CharFilter(field_name="unique_number")
+
+    unique_number__unique_number = django_filters.CharFilter(label="ID Number")
+    unique_number__first_name = django_filters.CharFilter(label="First Name")
+    unique_number__middle_name = django_filters.CharFilter(label="Middle Name")
+    unique_number__last_name = django_filters.CharFilter(label="Last Name")
+    unique_number__suffix = django_filters.CharFilter(label="Suffix")
 
     class Meta:
         model = Medicalcertificate_logbook
         fields = '__all__'
+        exclude = 'unique_number'
 
     def get_queryset(self):
         queryset = super().get_queryset()
