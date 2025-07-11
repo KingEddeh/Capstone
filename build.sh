460362dfa020e6cd424cb840d25da6c26db4235f
KingEddeh <28296038+KingEddeh@users.noreply.github.com>
2025-07-11 09:58:58 +0800

    add script for making superuser

--- build.sh
+++ build.sh
@@ -9,3 +9,17 @@ python manage.py collectstatic --no-input
 
 # Run database migrations
 python manage.py migrate
+
+# Create superuser automatically
+python manage.py shell -c "
+from django.contrib.auth.models import User;
+import os;
+username = 'admin';
+email = 'admin@dhvsu.edu.ph';
+password = 'admin';
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password);
+    print('Superuser created successfully');
+else:
+    print('Superuser already exists');
+"
