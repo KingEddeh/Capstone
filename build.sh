af122d4c4d490be84acf79b4fc6037d5a923f349
KingEddeh <28296038+KingEddeh@users.noreply.github.com>
2025-07-11 10:06:24 +0800

    remove new script

--- build.sh
+++ build.sh
@@ -10,16 +10,16 @@ python manage.py collectstatic --no-input
 # Run database migrations
 python manage.py migrate
 
-# Create superuser automatically
-python manage.py shell -c "
-from django.contrib.auth.models import User;
-import os;
-username = 'admin';
-email = 'admin@dhvsu.edu.ph';
-password = 'admin';
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password);
-    print('Superuser created successfully');
-else:
-    print('Superuser already exists');
-"
+# # Create superuser automatically
+# python manage.py shell -c "
+# from django.contrib.auth.models import User;
+# import os;
+# username = 'admin';
+# email = 'admin@dhvsu.edu.ph';
+# password = 'admin';
+# if not User.objects.filter(username=username).exists():
+#     User.objects.create_superuser(username, email, password);
+#     print('Superuser created successfully');
+# else:
+#     print('Superuser already exists');
+# "
